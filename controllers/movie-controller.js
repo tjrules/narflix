f2c098e9f9059fec046f399325fc84c5a1f95203
Emily Marro <emily.marro@gmail.com>
2018-01-30 15:46:14 -0500

    movies routes, controllers, db, model

--- controllers/movie-controller.js
+++ controllers/movie-controller.js
@@ -1,1 +1,90 @@
 //movie controller
+const Movie = require('../models/movie');
+const moviesController = {};
+
+moviesController.index = (req, res) => {
+  Movie.findAll(req.user.id)
+  .then(movie => {
+    res.render('movies/', { movies: movie });
+  })
+  .catch(err => {
+    res.status(400).json(err);
+  });
+};
+
+moviesController.new = (req, res) => {
+  res.render('movies/new')
+};
+
+moviesController.create = (req, res) => {
+  Movie.create({
+      imdb_id: req.body.imdb_id,
+      overview: req.body.overview,
+      poster_path: req.body.poster_path,
+      runtime: req.body.runtime,
+      tagline: req.body.tagline,
+      genres: req.body.genres,
+      user_id: req.user.id
+    })
+    .then(movie => {
+      res.redirect(`movies/${movie.id}`)
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+};
+
+moviesController.show = (req, res) => {
+  Movie.findById(req.params.id)
+    .then(movies => {
+      // console.log(moviescontrollershow);
+      res.render(`movies/show`, { movies:movies })
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+};
+
+
+moviesController.edit = (req,res) => {
+  Movie.findById(req.params.id)
+    .then(movies => {
+      res.render(`movies/edit`, {
+        goals:goals
+      })
+    })
+    .catch(err => {
+      res.status(400).json(err)
+    })
+}
+
+
+moviesController.update = (req, res) => {
+  Movie.update({
+      imdb_id: req.body.imdb_id,
+      overview: req.body.overview,
+      poster_path: req.body.poster_path,
+      runtime: req.body.runtime,
+      tagline: req.body.tagline,
+      genres: req.body.genres,
+    }, req.params.id)
+    .then( movies => {
+      res.redirect(`${req.params.id}`)
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+};
+
+
+moviesController.delete = (req, res) => {
+  Movie.delete(req.params.id)
+    .then(() => {
+      res.redirect('/')
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+};
+
+module.exports = moviesController;
--- models/movie.js
+++ models/movie.js
@@ -1,1 +1,58 @@
+const db = require('../db/config');
+
+const Movie = {};
+
+Movie.findAll = id => {
+  return db.query(`SELECT *, goals.id FROM goals JOIN users ON goals.user_id = users.id WHERE goals.user_id = $1`, id)
+};
+
+
+Movie.findById = id => {
+  return db.oneOrNone(`SELECT * FROM goals WHERE id = $1`, [id])
+};
+
+Movie.create = (movies) => {
+  console.log("created movie");
+  return db.one(
+    `
+      INSERT INTO movies
+      (imdb_id, overview, poster_path, runtime, tagline, genres, user_id)
+      VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *
+    `,
+    movies.imdb_id, movies.overview, movies.poster_path, movies.runtime, movies.tagline, movies.genres, movies.user_id]
+  );
+};
+
+
+Movie.update = (goals, id) => {
+  console.log("update working");
+  return db.none(
+  `
+    UPDATE movies SET
+    description = $1,
+    step1 = $2,
+    step2 = $3,
+    step3 = $4,
+    step4 = $5
+    WHERE id = $6
+  `,
+  [goals.description, goals.step1, goals.step2, goals.step3, goals.step4, id]
+    );
+};
+
+
+Movie.delete = id => {
+  console.log('model delete running');
+  return db.none(
+    `
+      DELETE FROM movies
+      WHERE id = $1
+    `,
+    [id]
+  );
+};
+
+
+
+module.exports = Movie;
 //movie model
