ebe875d4efa447b17300d96e3d64918283352345
TJ Stubbs <stubbsrule@gmail.com>
2018-01-30 15:47:34 -0500

    Created Favorite List file structure

--- src/components/FavoriteList.js
+++ src/components/FavoriteList.js
@@ -1,1 +1,41 @@
-// favorite list
+
+import React, {Component} from 'react';
+import axios, from 'axios';
+
+class FavoriteList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      apiDataLoaded: false,
+      apiData: null,
+    }
+  }
+  componentDidMount() {
+    axios.get('/narflixdb')
+      .then(res => {
+        this.setState({
+          apiDataLoaded:true,
+          apiData: res.data.data,
+        })
+      })
+  }
+
+  renderFavoriteList() {
+    if(this.state.apiDataLoaded) {
+      return this.state.apiData.map(favorites => {
+        return(
+          //add table data
+        )
+      })
+    }
+  }
+render() {
+
+  return(
+    <div>{this.renderFavoriteList}</div>
+    )
+  }
+
+}
+
+export default FavoriteLIst;
