28f8458b0de9f18831b6f052a77639f1a51e490e
Shazi Rahim <shaz.rahim@yahoo.com>
2018-01-31 11:33:44 -0500

    added a footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,12 @@
 //footer
+import React from 'react';
+
+const Footer = () => {
+    return(
+      <div className="Footer">
+        Made with <span role='img'>Heart</span>
+      </div>
+    )
+}
+
+export default Footer;
