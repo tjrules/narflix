aaad520827c3d75cc6451f5c2202eafbc10ef36e
Shazi Rahim <shaz.rahim@yahoo.com>
2018-01-31 12:19:08 -0500

    renders Home component which renders header and genre, need to render all other routes

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header'
+
+
+class Home extends React.Component {
+  render() {
+    return(
+      <div>
+        <Header />
+        <h2>this is the Home Component, we will be rendering Genres here</h2>
+
+      </div>
+    )
+  }
+}
+
+export default Home;
