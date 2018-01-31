1184e87a14194ce9cfa7dc836321f2334c472790
David Markoc <davidmarkoc@ool-ad02022c.dyn.optonline.net>
2018-01-31 11:43:01 -0500

    created show-movie component to render single movie

---
+++ src/components/ShowMovie.js
@@ -0,0 +1,20 @@
+// show movie
+
+import React from 'react';
+
+class ShowMovie extends Component {
+  render() {
+      return (
+        <div className="ShowMovie">
+          <div>{this.props.movie.title}</div>
+          <div>Tagline: {this.props.movie.tagline}</div>
+          <img src={`http://image.tmdb.org/t/p/w342${this.props.movie.poster_path}`} />
+          <div>Overview: {this.props.movie.overview}</div>
+          <div>IMDB ID: {this.props.movie.imdb_id}</div>
+          <div>Runtime: {this.props.movie.runtime} minutes</div>
+        </div>
+      )
+   }
+}
+
+export default ShowMovie;
