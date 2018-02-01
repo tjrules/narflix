b99e28f5119c591966e591f43d6e996c0d4b07e9
Emily Marro <emily.marro@gmail.com>
2018-02-01 14:36:01 -0500

    maybe fixed login

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
 const path = require('path');
+const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
+// set up port
+const PORT = process.env.PORT || 3000;
+// for authentication
+
 
 app.use(bodyParser.json());
 
@@ -15,6 +19,26 @@ app.use(express.static('build'));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'))
 });
+
+//set up passport local
+require('dotenv').config();
+
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+// allows user to bounce user auth info between all requests,
+// so user need not resign every new page.
+const session = require('express-session');
+// app.use(session({
+//   secret: process.env.SESSION_KEY,
+//   resave: false,
+//   saveUninitialized: true,
+// }));
+
+//set up passport local
+const passport = require('passport');
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/favorites', require('./routes/movie-routes'));
 app.use('/auth', require('./routes/auth-routes'))
