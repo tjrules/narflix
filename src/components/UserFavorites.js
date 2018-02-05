606ff761f4cd39a9158af7865236b688bdfd9db0
TJ Stubbs <stubbsrule@gmail.com>
2018-02-05 10:39:01 -0500

    fixed userFavorites

--- src/components/UserFavorites.js
+++ src/components/UserFavorites.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import deleteMovie from './deleteMovie';
 
 const UserFavorites = (props) => {
 
   return (
     <div className="user-fav">
-      <img src={props.movies.poster_path} alt="{props.movies.title}" />
-      <h2>{props.movies.title}</h2>
-      <p>Overview: {props.movies.overview}</p>
+
+      <form action="">
+        <img src={`http://image.tmdb.org/t/p/w342${props.movies.poster_path}`} alt="{props.movies.title}" />
+        <h2>{props.movies.title}</h2>
+        <p>Overview: {props.movies.overview}</p>
+
       <Link to={`/favorites/${props.movies.id}`}>See more</Link>
-       <Link to={`/edit/${props.match.params.id}`}>Edit</Link> 
+      <Link to={`/edit/${props.movies.id}`} moves={props.movies}>Edit</Link>
+      </form>
     </div>
   )
 }
